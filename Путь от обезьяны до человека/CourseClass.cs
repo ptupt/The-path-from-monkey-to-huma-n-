baec4b11b35209aa7acfd3e3b9bb6eb9848d7bca
ptupt <ebaltebqvrot@gmail.com>
2025-02-17 18:11:18 +0300

    Добавьте файлы проекта.

---
+++ Путь от обезьяны до человека/CourseClass.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 202.Дубровский.wdad.learn.course
+{
+    internal class CourseClass
+    {
+    }
+}
---
+++ Путь от обезьяны до человека/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace 202.Дубровский.wdad
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
